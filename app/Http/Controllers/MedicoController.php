bae3e50842acf52d8a70deaf328920eeda5472c4
Edyelgue Carneiro <edyelgue12@gmail.com>
2024-10-14 23:14:32 -0300

    criado rotas api CRUD para Medico

--- app/Http/Controllers/MedicoController.php
+++ app/Http/Controllers/MedicoController.php
@@ -12,4 +12,75 @@ class MedicoController extends Controller
         // Retorna todos os médicos no formato JSON
         return response()->json(Medico::all(), 200);
     }
+
+    public function store(Request $request)
+    {
+        // Armazena um novo medico
+        Medico::create([
+            'nome' => $request->nome,
+            'crm' => $request->crm,
+            'especialidade' => $request->especialidade,
+            'telefone' => $request->telefone,
+            'email' => $request->email,
+            'endereco' => $request->endereco,
+            'status' => '1',
+            'obs' => $request->obs
+        ]);
+
+        return response(['Ok'], 200);
+    }
+
+    public function show($id)
+    {
+        // Exibe um medico específico
+        // Busca o medico pelo ID
+        $medico = Medico::find($id);
+
+        // Verifica se o medico foi encontrado
+        if (!$medico) {
+            return response()->json(['message' => 'Medico não encontrado'], 404);
+        }
+
+        // Retorna o medico em formato JSON
+        return response()->json($medico, 200);
+    }
+
+    public function update(Request $request)
+    {
+        // Atualiza um medico existente
+        $medico = Medico::find($request->id);
+
+        $medico->nome = $request->nome;
+        $medico->crm = $request->crm;
+        $medico->especialidade = $request->especialidade;
+        $medico->telefone = $request->telefone;
+        $medico->email = $request->email;
+        $medico->endereco = $request->endereco;
+        $medico->obs = $request->obs;
+
+        $medico->save();
+
+        return response('Editado com sucesso!', 200);
+    }
+
+    public function soft_delete(Request $request)
+    {
+        // Atualiza um medico existente
+        $medico = Medico::find($request->id);
+
+        $medico->status = $request->status;
+
+        $medico->save();
+
+        return response('Removido com sucesso!', 200);
+    }
+
+    public function destroy($id)
+    {
+        // Remove um medico específico
+        $medico = Medico::find($id);
+        $medico->delete();
+
+        return response('Removido com sucesso!', 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MedicoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/medicos', [App\Http\Controllers\MedicoController::class, 'index']);
+Route::get('/medicos', [MedicoController::class, 'index']);
+Route::get('/medicos/{id}', [MedicoController::class, 'show']);
+Route::post('/medicos', [MedicoController::class, 'store']);
+Route::put('/medicos/{id}', [MedicoController::class, 'update']);
+Route::put('/medicos/{id}', [MedicoController::class, 'soft_delete']);
+Route::delete('/medicos/{id}', [MedicoController::class, 'destroy']);
